25a5102690cc31c456fa9947e4931d21f9ee95a4
Vikramsungadi <sungadivikram1@gmail.com>
2025-01-02 11:17:27 +0530

    feat: added carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  content: ['./**/*.liquid', './src/js/**/*.js', './src/css/**/*.css'],
+  content: ['./**/*.liquid', './assets/*.js', './assets/*.css'],
   theme: {
     extend: {
       maxWidth: {
         page: '1280px'
+      },
+      fontSize: {
+        h1: ['3.75rem', { lineHeight: '1.2' }],
+        h2: ['3rem', { lineHeight: '1.3' }],
+        h3: ['2.25rem', { lineHeight: '1.4' }],
+        h4: ['1.875rem', { lineHeight: '1.5' }],
+        h5: ['1.5rem', { lineHeight: '1.6' }],
+        h6: ['1.25rem', { lineHeight: '1.6' }]
       }
     }
   },
